685f076b30441a4e174e8c87cce595bdfa0b743f
Mike Schweitzer <mschweitzer@gmail.com>
2021-12-17 23:04:45 -0800

    Cleanup syntax of HelloWorld.Build.cs

--- HelloWorld/Source/HelloWorld/HelloWorld.Build.cs
+++ HelloWorld/Source/HelloWorld/HelloWorld.Build.cs
@@ -7,18 +7,19 @@ public class HelloWorld : ModuleRules
 	public HelloWorld(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", });
-		PublicDependencyModuleNames.AddRange(new string[] { "Json", "JsonUtilities", });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(new []
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+		});
 		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		PublicDependencyModuleNames.AddRange(new []
+		{
+			"Json",
+			"JsonUtilities",
+		});
 	}
 }
