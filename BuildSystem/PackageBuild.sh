6aab71cb2a49651414d973bae790e998ceeec0cf
Mike Schweitzer <mschweitzer@gmail.com>
2022-02-07 01:56:46 -0800

    Rearrange PackgedBuild params to make slightly more sense

--- BuildSystem/PackageBuild.sh
+++ BuildSystem/PackageBuild.sh
@@ -2,5 +2,5 @@
 BuildCookRun -project="$2" \
 -platform="$3" -noP4 \
 -clientconfig=Shipping -serverconfig=Shipping \
--build -nocompileeditor -SkipCookingEditorContent \
--cook -allmaps -stage -package -prereqs
+-build -nocompileeditor \
+-cook -SkipCookingEditorContent -allmaps -stage -package -prereqs
